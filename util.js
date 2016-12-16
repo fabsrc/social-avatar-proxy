ec92789af2acafe955f6a59126a17a3736e657b2
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-12-16 16:15:38 +0100

    Fix Lint Error, Update Packages

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 function getYouTubePicture (username, cb) {
-  let options = {
+  const options = {
     url: 'https://www.youtube.com/' + username,
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:44.0) Gecko/20100101 Firefox/44.0'
@@ -11,13 +11,13 @@ function getYouTubePicture (username, cb) {
   request(options, (err, res, body) => {
     if (err) console.error(err)
 
-    let match = body.match(/<img class="channel-header-profile-image" src="([\/\w\.:\-_]*)/)
+    const match = body.match(/<img class="channel-header-profile-image" src="([/\w.:\-_]*)/)
     return match ? cb(match[1]) : cb(null)
   })
 }
 
 function getFacebookId (username, cb) {
-  let options = {
+  const options = {
     url: 'https://facebook.com/' + username,
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:44.0) Gecko/20100101 Firefox/44.0'
@@ -27,7 +27,7 @@ function getFacebookId (username, cb) {
   request(options, (err, res, body) => {
     if (err) console.error(err)
 
-    let match = body.match(/entity_id":"(\d*)"/)
+    const match = body.match(/entity_id":"(\d*)"/)
     return match ? cb(match[1]) : cb(null)
   })
 }
